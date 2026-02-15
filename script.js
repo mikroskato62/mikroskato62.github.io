d4c866b2bc196cb608a34fab543a83fce4b1c877
RIP <petrakisraf2@gmail.com>
2026-02-15 18:09:33 +0200

    Random RGB generator.
    For the text hover effect (*) ...

--- script.js
+++ script.js
@@ -2,3 +2,24 @@
 let originalTitle = document.title;
 window.addEventListener("blur", () => { document.title = "Please come back ?"; });
 window.addEventListener("focus", () => { document.title = originalTitle; });
+
+// Random color generator for text hover effect ...
+const handleText = document.querySelector('.handle');
+let colorInterval;
+function setRandomColor() 
+{
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    document.documentElement.style.setProperty('--random-color', `rgb(${r}, ${g}, ${b})`);
+}
+handleText.addEventListener('mouseenter', () => 
+{
+    setRandomColor();
+    colorInterval = setInterval(setRandomColor, 500); 
+});
+handleText.addEventListener('mouseleave', () => 
+{
+    clearInterval(colorInterval);
+    document.documentElement.style.setProperty('--random-color', '#ffffff');
+}); 
